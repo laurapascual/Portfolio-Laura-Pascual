f94afaef8a721e62db381ac9feeede526dfa95d8
laurapascual <laurapascualmateo@gmail.com>
2023-04-07 20:15:47 +0200

    Add fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 /* SECCIÓN DE IMPORT */
-
-// - De React
-// - Nuestros
-// - Sass
 import '../styles/App.scss';
 // - Imágenes
 
-/* SECCIÓN DEL COMPONENTE */
-function App() {
+ /* SECCIÓN DEL COMPONENTE */
+
+const App = () => {
   /* VARIABLES ESTADO (DATOS) */
 
   /* EFECTOS (código cuando carga la página) */
@@ -17,10 +14,15 @@ function App() {
   /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
 
   /* HTML */
-  return <div className="App">{/* Aquí va el HTML */}</div>;
+return <div className="App">
+  <h1 className='title'>HOLA</h1></div>;
 }
 
 /* PROP-TYPES */
 
 /* EXPORT DEL COMPONENTE */
-export default App;
+export default App; 
+
+
+
+ 
