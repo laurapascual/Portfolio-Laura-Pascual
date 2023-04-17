b03d30fb1edec123605180df6775ba7d792d400d
laurapascual <laurapascualmateo@gmail.com>
2023-04-17 12:18:39 +0200

    Add shape and subtitle to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,15 @@
+import shape from '../images/shape.svg';
+
 const contact= () => {
     return(
         <div className='contact' id="contact">
             <h4 className="contact__title">CONTACT</h4>
+            <h5 className='contact__subtitle'>REACH OUT!</h5>
+            <img src={shape} alt="" className='contact__shape'/>
             <p className="contact__desc">To contact me you can do it through the following social networks or my email. I am always open to listen and receive feedback. Thank you!</p>
-            <ul>
-                <li className="contact__rrss">https://github.com/laurapascual</li>
-                <li className="contact__rrss">linkedin.com/in/laura-pascual-mateo/</li>
-                <li className="contact__rrss">laurapascualmateo@gmail.com</li>
-            </ul>
+                <i class="fa-brands fa-github-alt"></i><a href="https://github.com/laurapascual" className="contact__rrss">github.com/laurapascual</a>
+                <i class="fa-brands fa-linkedin-in"></i><a href="https://linkedin.com/in/laura-pascual-mateo/" className="contact__rrss">linkedin.com/in/laura-pascual-mateo/</a>
+                <i class="fa-regular fa-envelope"></i><a href="mailto:laurapascualmateo@gmail.com" className="contact__rrss">laurapascualmateo@gmail.com</a>
         </div>
     )
 }
