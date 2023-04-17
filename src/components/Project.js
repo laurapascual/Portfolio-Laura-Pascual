865e13a54f2bb58653cba419c30b9d35bad6c099
laurapascual <laurapascualmateo@gmail.com>
2023-04-17 21:19:50 +0200

    Add project scss

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,12 +5,13 @@ const project= ({eachProject}) => {
     return(
         <div className='project'>
             <img src={eachProject.image} alt="" className='project__image'/>
-            <h4 className='project__title'>{eachProject.name.toUpperCase()}</h4>
-            <div className="project__container">
-                <a href={eachProject.repo} Target="_blank"><i className="fa-brands fa-square-github project__icons"></i></a>
-                <a href={eachProject.demo} Target="_blank"><i class="fa-solid fa-arrow-up-right-from-square project__icons"></i></a>
+            <div className="project__row">
+                <h4 className='project__title'>{eachProject.name.toUpperCase()}</h4>
+                <div className="project__container">
+                    <a href={eachProject.repo} Target="_blank"><i className="fa-brands fa-square-github project__icons"></i></a>
+                    <a href={eachProject.demo} Target="_blank"><i class="fa-solid fa-arrow-up-right-from-square project__icons"></i></a>
+                </div>
             </div>
-            
             <div className="project__position">
                 {technologies}
             </div>
