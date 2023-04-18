e6421c87e7d1e222248bd68a73f76a0f2e675d4f
laurapascual <laurapascualmateo@gmail.com>
2023-04-18 15:49:41 +0200

    Update contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,12 +5,17 @@ const contact= () => {
             <div className='contact__section'> 
                 <p className="contact__desc">To contact me you can do it through the following social networks or my email. I am always open to listen and receive feedback. Thank you!</p>
                 <div className='contact__section2'>
-                    <i class="fa-brands fa-github-alt"></i><a href="https://github.com/laurapascual" className="contact__rrss">github.com/laurapascual</a>
-                    <i class="fa-brands fa-linkedin-in"></i><a href="https://linkedin.com/in/laura-pascual-mateo/" className="contact__rrss">linkedin.com/in/laura-pascual-mateo/</a>
-                    <i class="fa-regular fa-envelope"></i><a href="mailto:laurapascualmateo@gmail.com" className="contact__rrss">laurapascualmateo@gmail.com</a>
+                    <div className="contact__section2--container">
+                        <i class="fa-brands fa-github-alt"></i><a href="https://github.com/laurapascual" className="contact__rrss">github.com/laurapascual</a>
+                    </div>
+                    <div className="contact__section2--container">
+                        <i class="fa-brands fa-linkedin-in"></i><a href="https://linkedin.com/in/laura-pascual-mateo/" className="contact__rrss">linkedin.com/in/laura-pascual-mateo/</a>
+                     </div>   
+                    <div className="contact__section2--container">
+                        <i class="fa-regular fa-envelope"></i><a href="mailto:laurapascualmateo@gmail.com" className="contact__rrss">laurapascualmateo@gmail.com</a>
+                    </div>
                 </div>
             </div>
-           
         </div>
     )
 }
