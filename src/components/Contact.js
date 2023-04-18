e3f882285b0966995c5a84e2f9281c06e1794639
laurapascual <laurapascualmateo@gmail.com>
2023-04-18 15:43:24 +0200

    Fix contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,16 @@
-import shape from '../images/shape.svg';
-
 const contact= () => {
     return(
         <div className='contact' id="contact">
             <h4 className="contact__title">CONTACT</h4>
             <div className='contact__section'> 
-                <h5 className='contact__subtitle'>REACH OUT!</h5>
-                <img src={shape} alt="" className='contact__shape'/>
                 <p className="contact__desc">To contact me you can do it through the following social networks or my email. I am always open to listen and receive feedback. Thank you!</p>
+                <div className='contact__section2'>
+                    <i class="fa-brands fa-github-alt"></i><a href="https://github.com/laurapascual" className="contact__rrss">github.com/laurapascual</a>
+                    <i class="fa-brands fa-linkedin-in"></i><a href="https://linkedin.com/in/laura-pascual-mateo/" className="contact__rrss">linkedin.com/in/laura-pascual-mateo/</a>
+                    <i class="fa-regular fa-envelope"></i><a href="mailto:laurapascualmateo@gmail.com" className="contact__rrss">laurapascualmateo@gmail.com</a>
+                </div>
             </div>
-            <div className='contact__section2'>
-                <i class="fa-brands fa-github-alt"></i><a href="https://github.com/laurapascual" className="contact__rrss">github.com/laurapascual</a>
-                <i class="fa-brands fa-linkedin-in"></i><a href="https://linkedin.com/in/laura-pascual-mateo/" className="contact__rrss">linkedin.com/in/laura-pascual-mateo/</a>
-                <i class="fa-regular fa-envelope"></i><a href="mailto:laurapascualmateo@gmail.com" className="contact__rrss">laurapascualmateo@gmail.com</a>
-            </div>
+           
         </div>
     )
 }
