ddfe349bc212a563fc3c04a44e7f81fdaf5b16b4
laurapascual <laurapascualmateo@gmail.com>
2023-04-23 23:22:26 +0200

    Add compiled file

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,9 @@ import Landing from './Landing';
 import Menu from './Menu';
 import Projects from './Projects';
 import Contact from './Contact';
-import harry from '../images/projectImages/harry.PNG';
-import buy from '../images/projectImages/buy.PNG';
-import cocktails from '../images/projectImages/cocktails.PNG';
-import friends from '../images/projectImages/friends.PNG';
-import compass from '../images/projectImages/brujula.PNG';
-import number from '../images/projectImages/number.PNG'
-import cards from '../images/projectImages/cards.PNG';
-import projects from '../images/projectImages/projects.PNG';
-import potter from '../images/overImages/potter.PNG';
-import project from '../images/overImages/project.PNG';
-import card from '../images/overImages/card.PNG';
-import cocktail from '../images/overImages/cocktail.PNG';
-import we from '../images/overImages/we.PNG';
-import quotes from '../images/overImages/quotes.PNG';
-import guess from '../images/overImages/guess.PNG';
-import comp from '../images/overImages/compass.PNG';
-import portfolio from '../images/projectImages/portfolio.PNG';
-import port from '../images/overImages/port.PNG';
 import Footer from './Footer';
 import { useTranslation } from "react-i18next";
+import { harry, buy, cocktails, friends, potter, project, card, cocktail, we, quotes, guess, comp, compass, number, portfolio, port, cards, projects } from './compiled';
 
 const App = () => {
   const projectList = [{
---
+++ src/components/compiled.js
@@ -0,0 +1,39 @@
+import harry from '../images/projectImages/harry.PNG';
+import buy from '../images/projectImages/buy.PNG';
+import cocktails from '../images/projectImages/cocktails.PNG';
+import friends from '../images/projectImages/friends.PNG';
+import compass from '../images/projectImages/brujula.PNG';
+import number from '../images/projectImages/number.PNG';
+import cards from '../images/projectImages/cards.PNG';
+import projects from '../images/projectImages/projects.PNG';
+import potter from '../images/overImages/potter.PNG';
+import project from '../images/overImages/project.PNG';
+import card from '../images/overImages/card.PNG';
+import cocktail from '../images/overImages/cocktail.PNG';
+import we from '../images/overImages/we.PNG';
+import quotes from '../images/overImages/quotes.PNG';
+import guess from '../images/overImages/guess.PNG';
+import comp from '../images/overImages/compass.PNG';
+import portfolio from '../images/projectImages/portfolio.PNG';
+import port from '../images/overImages/port.PNG';
+
+export { 
+  harry,
+  buy,
+  cocktails,
+  friends,
+  compass,
+  number,
+  cards,
+  projects,
+  potter,
+  project,
+  card,
+  cocktail,
+  we,
+  quotes,
+  guess,
+  comp,
+  portfolio,
+  port
+};
